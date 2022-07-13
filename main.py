eb9ac25bdbffc3d4931242d8ff04c858d87c5df6
Stalomir <slawomir.kapinos@gmail.com>
2022-07-13 13:54:24 +0200

    Module 10 complete

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    movies = tmdb_client.get_movies(how_many=9)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get("list_type", "popular")
+    query_string=request.query_string
+    available_lists = ['now_playing','popular','top_rated','upcoming']
+    for list in available_lists:
+        if list == selected_list:
+            break
+    else:
+        selected_list = "popular"
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, available_lists=available_lists, selected_list=selected_list, query_string=query_string)
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
 @app.context_processor
 def utility_processor():
-    def tmdb_poster_url(path, size):
-        return tmdb_client.get_poster_urls(path, size)
-    return {"tmdb_poster_url": tmdb_poster_url}
+    def tmdb_picture_url(path, size):
+        return tmdb_client.get_picture_urls(path, size)
+    return {"tmdb_picture_url": tmdb_picture_url}   
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -2,21 +2,48 @@ from flask import Flask
 import requests
 import random
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Y2FiOGMyMjEzZmM0MjFjYmQ1Yzk3ZDE4OGU2MmRlOCIsInN1YiI6IjYyYmM3Yjk1ZTFmYWVkMWRjNzc2OWMxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gT60CG_IgbCzJG3PMjGMWEuc4eb86-7yIJaGhR-DEv4"
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'alamakota'
 
 def get_popular_movies():
-    url = "https://api.themoviedb.org/3/movie/popular"
-    headers = {'Authorization': "Bearer %s" % "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Y2FiOGMyMjEzZmM0MjFjYmQ1Yzk3ZDE4OGU2MmRlOCIsInN1YiI6IjYyYmM3Yjk1ZTFmYWVkMWRjNzc2OWMxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gT60CG_IgbCzJG3PMjGMWEuc4eb86-7yIJaGhR-DEv4"}
-    r =  requests.get(url, headers=headers)
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    headers = {'Authorization': "Bearer %s" % API_TOKEN}
+    r =  requests.get(endpoint, headers=headers)
     return r.json()
 
-def get_poster_urls(poster_api_path, size):
+def get_picture_urls(picture_api_path, size):
     tmdb_url = "https://image.tmdb.org/t/p/"
-    poster_url = f"{tmdb_url}{size}{poster_api_path}"
-    return poster_url
+    picture_url = f"{tmdb_url}{size}{picture_api_path}"
+    return picture_url
 
-def get_movies(how_many):
-    data = get_popular_movies()
-    random.shuffle(data['results'])
-    return data["results"][:how_many]
+def get_movies(how_many, list_type):
+    data = get_movie_list(list_type)["results"]
+    random.shuffle(data)
+    return data[:how_many]
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {'Authorization': "Bearer %s" % API_TOKEN}
+    r =  requests.get(endpoint, headers=headers)
+    return r.json()
+
+def get_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {'Authorization': "Bearer %s" % API_TOKEN}
+    r =  requests.get(endpoint, headers=headers)
+    return r.json()["cast"][:6]
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {'Authorization': f"Bearer {API_TOKEN}"}
+    r = requests.get(endpoint, headers=headers)
+    return r.json()
+
+def get_movie_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = { "Authorization": f"Bearer {API_TOKEN}"}
+    r = requests.get(endpoint, headers=headers)
+    r.raise_for_status()
+    return r.json()
